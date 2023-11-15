c6443f9870bba2c6a7dfd19c68e8e76a26082487
Kiran Anand <liteninkiran@hotmail.com>
2023-11-15 23:30:21 +0000

    013 - Adding a DbContext class

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,13 @@
+using API.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 app.UseHttpsRedirection();

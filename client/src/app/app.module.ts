a509fa2b7156f11eb06fc277eba4a3efdceb8040
Kiran Anand <liteninkiran@hotmail.com>
2023-11-18 18:11:00 +0000

    024 - Making HTTP requests in Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
+        HttpClientModule,
     ],
     providers: [],
     bootstrap: [AppComponent],

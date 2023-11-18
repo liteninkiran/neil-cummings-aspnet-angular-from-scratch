4b11f459b2fdb62f6372ac7c3707b79384fc3f83
Kiran Anand <liteninkiran@hotmail.com>
2023-11-18 19:08:31 +0000

    031 - Saving into source control

--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,6 @@ builder.Services.AddDbContext<DataContext>(opt => {
 });
 builder.Services.AddCors();
 var app = builder.Build();
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 app.Run();

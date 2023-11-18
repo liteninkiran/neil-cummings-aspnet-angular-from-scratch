7a623901a6151738c46e41ada0b53823a87cef28
Kiran Anand <liteninkiran@hotmail.com>
2023-11-18 18:16:09 +0000

    025 - Adding CORS support in the API

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,13 @@
 using API.Data;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 app.Run();

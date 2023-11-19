c1b68a9f9351fd904d7918aee93124c4083e25e8
Kiran Anand <liteninkiran@hotmail.com>
2023-11-19 16:51:09 +0000

    051 - Introduction to Angular template forms

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
     styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
+    public model: any = {}
+
+    constructor() {
+
+    }
+
+    public ngOnInit(): void {
+
+    }
+
+    public login(): void {
+        console.log(this.model);
+    }
 }

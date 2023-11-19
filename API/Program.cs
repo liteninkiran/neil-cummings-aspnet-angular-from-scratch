d2d3fa2e72d769fb9b9558110e2fe5ff4a68e0e5
Kiran Anand <liteninkiran@hotmail.com>
2023-11-19 13:35:00 +0000

    046 - Adding the authentication middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
 using API;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -9,7 +12,19 @@ builder.Services.AddDbContext<DataContext>(opt => {
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+    var tokenKey = Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"]);
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(tokenKey),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+});
 var app = builder.Build();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();

d4ae659d0e61e0ac26638c7fa0a0f85b928a5290
Kiran Anand <liteninkiran@hotmail.com>
2023-11-21 15:40:10 +0000

    098 - Configuring AutoMapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -8,7 +8,14 @@ public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
     {
-        CreateMap<AppUser, MemberDto>();
+        CreateMap<AppUser, MemberDto>().ForMember(
+            dest => dest.PhotoUrl,
+            optn => optn.MapFrom(
+                src => src.Photos.FirstOrDefault(
+                    photo => photo.IsMain
+                ).Url
+            )
+        );
         CreateMap<Photo, PhotoDto>();
     }
 }

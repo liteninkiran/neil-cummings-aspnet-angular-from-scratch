0b5b3d55d469ab95a395700264f08406e08a114d
Kiran Anand <liteninkiran@hotmail.com>
2024-01-18 13:27:40 +0000

    148 - Using the form builder service

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { HttpErrorResponse } from '@angular/common/http';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
@@ -19,6 +19,7 @@ export class RegisterComponent implements OnInit {
     constructor(
         private accountService: AccountService,
         private toastr: ToastrService,
+        private fb: FormBuilder,
     ) { }
 
     public ngOnInit(): void {
@@ -41,17 +42,17 @@ export class RegisterComponent implements OnInit {
     }
 
     private initialiseForm(): void {
-        this.registerForm = new FormGroup({
-            username: new FormControl('', Validators.required),
-            password: new FormControl('', [
+        this.registerForm = this.fb.group({
+            username: ['', Validators.required],
+            password: ['', [
                 Validators.required,
                 Validators.minLength(4),
                 Validators.maxLength(8),
-            ]),
-            confirmPassword: new FormControl('', [
+            ]],
+            confirmPassword: ['', [
                 Validators.required,
                 this.matchValues('password'),
-            ]),
+            ]],
         });
 
         this.registerForm
